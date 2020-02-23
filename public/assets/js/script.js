f0d2d22e5c81a03e310f14fc466e033226eb2487
marif0025 <marif0025@gmail.com>
2020-02-23 19:20:40 +0500

    Services Website, Initial HTML CSS Completed

---
+++ public/assets/js/script.js
@@ -0,0 +1,10 @@
+const toTop = document.querySelector('.btt');
+
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset > 700) {
+        toTop.classList.add('visible');
+    }
+    else {
+        toTop.classList.remove('visible');
+    }
+})
